bce17fa71d48ca64e9862532e5c1ec246e714864
Amit Kumar Parouha <86564305+AmitParouha@users.noreply.github.com>
2026-02-17 10:49:12 +0530

    Added println command

--- Demo.java
+++ Demo.java
@@ -1,5 +1,7 @@
 public class Demo{
   public static void main(String [] args){
+    System.out.println("Hello from Jenkins!");
+    System.out.println("This code is pulled from Git repository.");
     System.out.println("Application started successfully.");
     System.out.println("Initializing application modules...");
     System.out.println("Loading configuration properties...");
