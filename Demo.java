c1583fc56822a215a5292c0cf032d40d1ab1ab0b
Amit Kumar Parouha <86564305+AmitParouha@users.noreply.github.com>
2026-02-17 11:48:48 +0530

    make changes in syntax

--- Demo.java
+++ Demo.java
@@ -5,7 +5,7 @@ public class Demo{
     System.out.println("Application started successfully.");
     System.out.println("Initializing application modules...");
     System.out.println("Loading configuration properties...");
-    System.out.println("Application is running...");
+    System.out.println("Application is running...")
   }
 
 }
